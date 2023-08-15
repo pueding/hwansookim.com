7b36bf2114ae085497ca4f68cf4c1f9761c21f92
Hwansoo <pueding@gmail.com>
2023-08-16 00:32:07 +0900

    feat: wip

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,20 +6,20 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "My Site",
-  tagline: "Dinosaurs are cool",
-  favicon: "img/favicon.ico",
+  title: "Hello, I'm Hwansoo",
+  tagline: "Software Engineer",
+  // favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-test-site.com",
+  url: "https://hwansookim.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "pueding", // Usually your GitHub org/user name.
+  projectName: "hwansookim.com", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
@@ -77,11 +77,11 @@ const config = {
       // Replace with your project's social card
       image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: "My Site",
-        logo: {
-          alt: "My Site Logo",
-          src: "img/logo.svg",
-        },
+        title: "Hwansoo Kim",
+        // logo: {
+        //   alt: "My Site Logo",
+        //   src: "img/logo.svg",
+        // },
         items: [
           {
             type: "docSidebar",
@@ -90,58 +90,58 @@ const config = {
             label: "Tutorial",
           },
           { to: "/blog", label: "Blog", position: "left" },
-          {
-            href: "https://github.com/facebook/docusaurus",
-            label: "GitHub",
-            position: "right",
-          },
+          // {
+          //   href: "https://github.com/facebook/docusaurus",
+          //   label: "GitHub",
+          //   position: "right",
+          // },
         ],
       },
-      footer: {
-        style: "dark",
-        links: [
-          {
-            title: "Docs",
-            items: [
-              {
-                label: "Tutorial",
-                to: "/docs/intro",
-              },
-            ],
-          },
-          {
-            title: "Community",
-            items: [
-              {
-                label: "Stack Overflow",
-                href: "https://stackoverflow.com/questions/tagged/docusaurus",
-              },
-              {
-                label: "Discord",
-                href: "https://discordapp.com/invite/docusaurus",
-              },
-              {
-                label: "Twitter",
-                href: "https://twitter.com/docusaurus",
-              },
-            ],
-          },
-          {
-            title: "More",
-            items: [
-              {
-                label: "Blog",
-                to: "/blog",
-              },
-              {
-                label: "GitHub",
-                href: "https://github.com/facebook/docusaurus",
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
+      // footer: {
+      //   style: "dark",
+      //   links: [
+      //     {
+      //       title: "Docs",
+      //       items: [
+      //         {
+      //           label: "Tutorial",
+      //           to: "/docs/intro",
+      //         },
+      //       ],
+      //     },
+      //     {
+      //       title: "Community",
+      //       items: [
+      //         {
+      //           label: "Stack Overflow",
+      //           href: "https://stackoverflow.com/questions/tagged/docusaurus",
+      //         },
+      //         {
+      //           label: "Discord",
+      //           href: "https://discordapp.com/invite/docusaurus",
+      //         },
+      //         {
+      //           label: "Twitter",
+      //           href: "https://twitter.com/docusaurus",
+      //         },
+      //       ],
+      //     },
+      //     {
+      //       title: "More",
+      //       items: [
+      //         {
+      //           label: "Blog",
+      //           to: "/blog",
+      //         },
+      //         {
+      //           label: "GitHub",
+      //           href: "https://github.com/facebook/docusaurus",
+      //         },
+      //       ],
+      //     },
+      //   ],
+      //   copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      // },
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,110 @@
-import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
 
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary','bg-blue-500', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
+// function HomepageHeader() {
+//   const {siteConfig} = useDocusaurusContext();
+//   return (
+//     <header className={clsx('hero hero--primary','bg-blue-500', styles.heroBanner)}>
+//       <div className="container">
+//         <h1 className="hero__title">{siteConfig.title}</h1>
+//         <p className="hero__subtitle">{siteConfig.tagline}</p>
+//         <div className={styles.buttons}>
+//           <Link
+//             className="button button--secondary button--lg"
+//             to="/docs/intro">
+//             Docusaurus Tutorial - 5min ⏱️
+//           </Link>
+//         </div>
+//       </div>
+//     </header>
+//   );
+// }
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
+      title={siteConfig.title}
+      description="Description will go into a meta tag in <head />"
+    >
       <main>
-        <HomepageFeatures />
+        <div className="container py-20 flex flex-col">
+          {/* Avatar */}
+          <div className="avatar mb-10">
+            <div className="w-24 rounded-full">
+              <img src="/imgs/profile.png" />
+            </div>
+          </div>
+          {/* Title */}
+          <div className="prose prose-gray max-w-none mb-10">
+            <h1 className="my-0">Hwansoo Kim</h1>
+            <h3 className="mt-1">
+              who loves software development and people around me
+            </h3>
+          </div>
+          {/* Description */}
+          <div className="prose mb-10">
+            <p>
+              Hwansoo Kim is a seasoned software engineer and technical lead
+              with extensive experience spanning various areas of software
+              engineering. This includes expertise in web, mobile, cloud
+              infrastructure, data engineering, and IoT products. His career has
+              consistently demonstrated his ability to analyze business
+              requirements and develop optimal software solutions, while also
+              excelling in leadership roles, leading technical teams, and
+              managing software development lifecycles.
+            </p>
+          </div>
+          {/* Carousel */}
+          <div className="mb-10">
+            <div className="carousel h-56 w-full">
+              <div className="carousel-item">
+                <img src="/imgs/futureplay.jpg" alt="Burger" />
+              </div>
+              <div className="carousel-item">
+                <img src="/imgs/honeybees.jpg" alt="Burger" />
+              </div>
+              <div className="carousel-item">
+                <img src="/imgs/wahome.jpg" alt="Burger" />
+              </div>
+              <div className="carousel-item">
+                <img src="/imgs/8rowing.jpg" alt="Burger" />
+              </div>
+            </div>
+          </div>
+          {/* Stats */}
+          <div className="stats shadow w-[50rem] mb-10">
+            <div className="stat">
+              <div className="stat-title">Companies I Joined</div>
+              <div className="stat-value">5</div>
+              <div className="stat-desc">From Jul 2009</div>
+            </div>
+
+            <div className="stat">
+              <div className="stat-title">Startups I Cofounded</div>
+              <div className="stat-value">4</div>
+              <div className="stat-desc">From Jul 2012</div>
+            </div>
+          </div>
+          {/* History */}
+          <div>
+            <div className="prose">
+              <h3>
+                Feb 2023 ~ Current - Director of Engineering at STCLab, Inc
+              </h3>
+              <p>
+                Leading and developing cloud-native products for autoscaling
+                across various platforms using Rust.
+              </p>
+            </div>
+          </div>
+        </div>
       </main>
     </Layout>
   );

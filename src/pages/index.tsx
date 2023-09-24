155ec90890db4a25648ffbd3622dca2abfd0d40d
Hwansoo <pueding@gmail.com>
2023-09-24 13:58:13 +0900

    feat: add "interested in"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,18 +33,23 @@ function HistoryItem(props: {
   children?: React.ReactNode;
 }) {
   return (
-    <div className="prose flex mb-10">
-      <div className="w-40 flex-none mr-10">
-        <p className="prose-gray my-0">{props.date}</p>
-        <p className="prose-gray my-0">{props.location}</p>
-      </div>
-
+    <div className="prose flex mb-10 md:flex-row flex-col max-w-full">
       <div className="flex-1">
-        <h3 className="mt-0">{props.title}</h3>
-        {props.children}
+        <h3 className="mt-0 mb-1 text-lg leading-5">{props.title}</h3>
+        <div className="flex flex-row">
+          <p className="prose-gray my-0 md:text-sm text-xs">{props.date}</p>
+          <p className="prose-gray my-0 md:text-sm text-xs md:ml-0 ml-4">
+            {props.location}
+          </p>
+        </div>
+        <div className="text-sm mt-3">{props.children}</div>
       </div>
     </div>
   );
+}
+
+function InterestingIcon(props: { src: string }) {
+  return <img src={props.src} className="max-w-10 max-h-10 m-0 mr-1" />;
 }
 
 export default function Home(): JSX.Element {
@@ -56,18 +61,31 @@ export default function Home(): JSX.Element {
     >
       <main>
         <div className="container py-20 flex flex-col">
-          {/* Avatar */}
-          <div className="avatar mb-10">
-            <div className="w-52 rounded-full">
-              <img src="/imgs/profile.png" />
+          {/* Avatar and Title*/}
+          <div className="flex md:flex-row flex-col items-center mb-20">
+            {/* Avatar */}
+            <div className="avatar mb-4 md:mb-0">
+              <div className="w-40 rounded-full">
+                <img src="/imgs/profile.png" />
+              </div>
             </div>
-          </div>
-          {/* Title */}
-          <div className="prose prose-gray max-w-none mb-10">
-            <h1 className="my-0">Hwansoo Kim</h1>
-            <h3 className="mt-1">
-              who loves software development and people around me
-            </h3>
+            {/* Title */}
+            <div className="prose prose-gray max-w-none md:ml-10 md:text-left text-center">
+              <h1 className="my-0">Hwansoo Kim</h1>
+              <h3 className="md:mt-1 my-0">
+                who loves software development and people around me and
+              </h3>
+              <div className="mt-1 flex md:flex-row flex-col">
+                <h3 className="my-0">is interested in</h3>
+                <div className="flex flex-1 md:justify-start justify-center items-center ml-4">
+                  <InterestingIcon src="/imgs/rust-icon.png" />
+                  <InterestingIcon src="/imgs/cncf.jpeg" />
+                  <InterestingIcon src="/imgs/k8s.png" />
+                  <InterestingIcon src="/imgs/gc.png" />
+                  <InterestingIcon src="/imgs/aws.png" />
+                </div>
+              </div>
+            </div>
           </div>
           {/* Description */}
           <div className="prose mb-10">
@@ -130,14 +148,24 @@ export default function Home(): JSX.Element {
             </div>
           </div> */}
           {/* History */}
-          <div>
+          <div className="w-full">
+            <HistoryItem
+              title="Technical Advisor at Presidential Committee on the Digital Platform Government"
+              date="Oct 2023 ~ Current"
+              location="Seoul, Korea"
+            >
+              <p className="my-0">
+                • Advising the government on the cloud digital transformation of
+                government services.
+              </p>
+            </HistoryItem>
             <HistoryItem
               title="Director of Engineering at STCLab, Inc"
               date="Feb 2023 ~ Current"
               location="Seoul, Korea"
             >
-              <p>
-                Leading and developing cloud-native products for autoscaling
+              <p className="my-0">
+                • Leading and developing cloud-native products for autoscaling
                 across various platforms using Rust.
               </p>
             </HistoryItem>
@@ -146,16 +174,16 @@ export default function Home(): JSX.Element {
               date="Jan 2021 ~ Jan 2023"
               location="Seoul, Korea"
             >
-              <p>
+              <p className="my-0">
                 • Led and developed the digital transformation of the startup
                 accelerator developing online services with our dev team.
               </p>
-              <p>
+              <p className="my-0">
                 • Enhanced the software development and the lifecycle by using
                 trending tech stacks and methodologies like Relay(GraphQL) with
                 Hasura, AWS serverless stacks, and BDD(Cucumber.js)
               </p>
-              <p>
+              <p className="my-0">
                 • Provided technical consulting for portfolio companies we've
                 invested in to help their growth.
               </p>
@@ -170,7 +198,7 @@ export default function Home(): JSX.Element {
                   target="_blank"
                   className="text-blue-500"
                 >
-                  Read more on Linkedin
+                  See more on Linkedin
                 </a>
               </p>
             </div>
